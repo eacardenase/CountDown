77f4b114005bbabf1b3213c50cc1835e3a40e006
Edwin Cardenas <eacardenase@gmail.com>
2024-12-29 18:30:29 -0500

    feat: add user input on count start

--- CountDown/main.c
+++ CountDown/main.c
@@ -6,9 +6,16 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#import <readline/readline.h>
 
 int main(int argc, const char * argv[]) {
-    for (int i = 99; i >= 0; i -= 3) {
+    printf("Where should I start counting? ");
+    
+    const char *strNumber = readline(NULL);
+    int number = atoi(strNumber);
+    
+    for (int i = number; i >= 0; i -= 3) {
         printf("%d\n", i);
         
         if (i % 5 == 0) {
